fbfe93bc6031dadf574da87fbb4c3036fa41975c
juan felipe oquendo lopez <felipeoquendo@hotmail.com>
2024-08-19 21:22:20 -0500

    add countries entity

--- Fantasy/Fantasy.Frontend/Program.cs
+++ Fantasy/Fantasy.Frontend/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after"); /*Prueba*/
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
