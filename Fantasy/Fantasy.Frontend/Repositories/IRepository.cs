9ddcc5b2e6b66fa26d4172ce6a740015722ae842
juan felipe oquendo lopez <felipeoquendo@hotmail.com>
2024-09-06 00:17:00 -0500

    Edit and delete countries

--- Fantasy/Fantasy.Frontend/Repositories/IRepository.cs
+++ Fantasy/Fantasy.Frontend/Repositories/IRepository.cs
@@ -2,6 +2,12 @@
 
 public interface IRepository
 {
+    Task<HttpResponseWrapper<object>> DeleteAsync(string url);
+
+    Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model);
+
+    Task<HttpResponseWrapper<TActionResponse>> PutAsync<T, TActionResponse>(string url, T model);
+
     //El <T> es generico y puede recibir diferentes clases y no tener que hacer una clase por cada servicio (repositorio generico para cuallquier tipo de dato)
     Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
 
