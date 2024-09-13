0a2cf618ef4537f7c0d8fd373c74e4750e44467d
juan felipe oquendo lopez <felipeoquendo@hotmail.com>
2024-09-12 22:42:42 -0500

    Add team entiti

---
+++ Fantasy/Fantasy.Backend/UnitsOfWork/Implementations/GenericUnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using Fantasy.Backend.UnitsOfWork.Interfaces;
+using Fantasy.Shared.Responses;
+
+namespace Fantasy.Backend.UnitsOfWork.Implementations;
+
+public class GenericUnitOfWork<T> : IGenericUnitOfWork<T> where T : class
+{
+    public Task<ActionResponse<T>> AddAsync(T model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ActionResponse<T>> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ActionResponse<IEnumerable<T>>> GetAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ActionResponse<T>> GetAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ActionResponse<T>> UpdateAsync(T model)
+    {
+        throw new NotImplementedException();
+    }
+}
